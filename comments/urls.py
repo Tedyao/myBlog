951e2b067ad93c3174b693350a495c8c56e9b8e4
TedYao <2433636090@qq.com>
2019-06-27 10:40:01 +0800

    first commit

---
+++ comments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from . import views
+
+app_name = 'comments'
+urlpatterns = [
+    path('comment/post/<int:post_pk>/', views.post_comment, name='post_comment'),
+
+]
